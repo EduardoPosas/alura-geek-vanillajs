95858cc5727d0fb9b52b27586dcfa039e20a04f4
Alexander Pozas <a.posas@outlook.com>
2023-09-27 20:14:23 -0600

    modifying base config options

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
+  base: '/alura-geek-vanillajs/',
   build: {
     rollupOptions: {
       input: {

1196c3e4e1abe1cf0e684478138b6c3bfb4ced37
Alexander Pozas <a.posas@outlook.com>
2023-09-27 22:36:21 -0600

    Updating Links

--- components/AdminBtn.js
+++ components/AdminBtn.js
@@ -2,7 +2,7 @@
 const AdminBtn = () => {
     const button = document.createElement('a');
     button.className = 'py-3 px-4 border border-indigo-600 text-indigo-600 font-bold rounded-sm hover:bg-indigo-800 hover:text-neutral-100';
-    button.href = '/views/products.html';
+    button.href = '/alura-geek-vanillajs/views/products.html';
     button.textContent = 'Admin';
 
     return button;

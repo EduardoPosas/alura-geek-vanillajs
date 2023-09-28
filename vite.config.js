953f893a1b93a677cf8ae3356e0b449f197e70a3
Alexander Pozas <a.posas@outlook.com>
2023-09-27 18:49:06 -0600

    deleting base option in config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  base: "/alura-geek-vanillajs/",
+  base: "/",
   build: {
     rollupOptions: {
       input: {

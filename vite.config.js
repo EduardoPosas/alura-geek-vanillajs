45f5d8b8ea20729009980d59b23ee0f04a528f2e
Alexander Pozas <a.posas@outlook.com>
2023-09-27 18:46:27 -0600

    specifying multiple html files en config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  base: "/alura-geek-vanillajs/"
+  base: "/alura-geek-vanillajs/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        add: resolve(__dirname, 'views/add-product.html'),
+        login: resolve(__dirname, 'views/login.html'),
+        description: resolve(__dirname, 'views/product-description.html'),
+        category: resolve(__dirname, 'views/products-category.html'),
+        products: resolve(__dirname, 'views/products.html'),
+      },
+    }
+  }
 })
